0177a6a9decf6ad7d9e430d5b99d42bb92193c5c
adel <86808805+elithaxxor@users.noreply.github.com>
2025-04-02 20:01:32 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -9,8 +9,20 @@ brew update
 
 # Install CLI Tools
 brew install afflib aircrack-ng apktool arping autopsy bettercap binwalk bulk_extractor cabextract cadaver chkrootkit crunch cutter darkstat dc3dd dcfldd ddrescue dex2jar dns2tcp dnsmap dnstracer ettercap exiv2 exploitdb fcrackzip foremost fping fragroute freerdp gdb hashcat hping httrack hydra ike-scan jad jd-gui john libewf libpst llvm lynis mac-robber masscan md5deep mdbtools metasploit mitmproxy nasm ncrack nikto nmap ophcrack owasp-zap p0f p7zip pdfcrack pev proxychains-ng proxytunnel ptunnel pwnat radare2 reaver recon-ng rkhunter siege sipp sipsak sleuthkit slowhttptest smali sqlmap ssdeep ssldump sslh sslscan sslsplit sslyze stunnel swaks tcpdump tcpflow tcpreplay theharvester truecrack udptunnel util-linux volatility wireshark yara 
+#!/bin/bash
+
+# Update database
+brew update
+
+# Install more CLI Tools
+brew install afflib aircrack-ng apktool arping autopsy bettercap binwalk bulk_extractor cabextract cadaver chkrootkit crunch darkstat dc3dd dcfldd ddrescue dex2jar dns2tcp dnsmap dnstracer ettercap exiv2 exploitdb fcrackzip foremost fping fragroute freerdp gdb hashcat hping httrack hydra ike-scan john libewf libpst llvm lynis mac-robber masscan md5deep mdbtools mitmproxy nasm ncrack nikto nmap ophcrack owasp-zap p0f p7zip pdfcrack pev proxychains-ng proxytunnel ptunnel pwnat radare2 reaver recon-ng rkhunter siege sipp sipsak sleuthkit slowhttptest smali sqlmap ssdeep ssldump sslh sslscan sslsplit sslyze stunnel swaks tcpdump tcpflow tcpreplay theharvester truecrack udptunnel util-linux volatility yara testssl sshtrix gobuster pwntools snort osquery zeek cowpatty amass ffuf dirsearch subfinder nuclei trufflehog semgrep bandit wpscan whatweb wafw00f netcat socat
 
 # Install GUI Tools
+brew install --cask armitage burp-suite cutter jad jd-gui maltego metasploit wireshark zenmap ghidra
+
+# Install Further Tools
+brew install openvpn tor
+# Install GUI more Tools
 brew install armitage burp-suite cutter jad maltego metasploit wireshark zenmap 
 
 # Install Further Tools
